8f166b9fad202ae58dcbd1aa7abb521c58c8343d
marcsoftware <marcdavidmelcher@gmail.com>
2021-08-06 10:50:23 -0500

    edited JSON name, label also turns red now, and now prompts user to login if they are not loggeed in

--- app/index.js
+++ app/index.js
@@ -2,11 +2,18 @@ import document from "document";
 import * as messaging from "messaging";
 import { user } from "user-profile";
 import { today } from "user-activity";
+import { me as appbit } from "appbit";
 
 // Message is received from companion
+
+
+//------------------------------------------------
+//
+//------------------------------------------------
 messaging.peerSocket.onmessage = evt => {
     //
-    console.log('from app is this executed');
+ 
+   renderPrompt();   
    let date = new Date();
   let hours = date.getHours(); //YYYY-MM-DD
   let minutes = date.getMinutes();
@@ -30,8 +37,30 @@ messaging.peerSocket.onmessage = evt => {
   if(balance<0){
   
     document.getElementById("balancecalories").style.fill='red';
+    document.getElementById("balancecalorieslabel").style.fill='red';
      }
   document.getElementById("BMR").text = BMR; 
   document.getElementById("balancecalories").text = balance; 
-
+ if(food){
+  clearPrompt(); //clear prompt if recieved message
+ }
 };
+
+//------------------------------------------------
+//
+//------------------------------------------------
+function clearPrompt(){
+  console.log('clear=============================clear');
+  document.getElementById("prompt1").text = ''; 
+  document.getElementById("prompt2").text = ''; 
+}
+
+
+//------------------------------------------------
+// Prompt user to login.
+//------------------------------------------------
+function renderPrompt(){
+  console.log('render=============================render');
+  document.getElementById("prompt1").text = 'You need to login'; 
+  document.getElementById("prompt2").text = 'see documentation'; 
+}
